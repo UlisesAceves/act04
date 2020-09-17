9f4a7ddac596dbddc894194830ce7d0f5cebc622
UlisesAceves <ulises.aceves7480@alumnos.udg.mx>
2020-09-16 19:29:52 -0500

    Captura Suma y Promedio de los 5 enteros funcionando

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+void capturar_enteros(){
+    int enteros[5];
+    float promedio;
+    int suma = 0;
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        scanf("%i", &enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%i\n", enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        suma += enteros[i]; //suma = suma + enteros[i]
+    }
+    printf("Suma: %i\n", suma);
+    printf("Promedio: %f\n", suma/5.0);
+}
+
 int main() {
     char op;
 
@@ -10,6 +31,19 @@ int main() {
         printf("0) Salir\n");
         scanf("%c", &op);
         fflush(stdin);
+
+        switch (op)
+        {
+        case '1':
+            capturar_enteros();
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        default:
+            break;
+        }
 
     }while(op != '0');
 
